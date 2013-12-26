b788cec283323665efd5b9c03c834c888e6c21ba
sithanm <none.none@gmx.de>
2013-12-26 14:25:22 +0100

    Revert "Server and Client"
    This reverts commit 2b4505efb119a961d75f7f0d770625755ee5d48d.

---
+++ main.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.net.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Main {
+	JTextArea eingehend;
+	JTextField ausgehend;
+	BufferedReader reader;
+	PrintWriter writer;
+	Socket sock;
+
+	public static void main(String[] args) {
+		Main client = new Main();
+		client.los();
+	}
+
+	public void los() {
+		JFrame frame= new JFrame("Laecherlich einfacher Chat-Client");
+		JPanel hauptPanel = new JPanel();
+		eingehend = new JTextArea(15,20);
+		eingehend.setLineWrap(true);
+		eingehend.setWrapStyleWord(true);
+		eingehend.setEditable(false);
+		JScrollPane fScroller = new JScrollPane(eingehend);
+		fScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		fScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		ausgehend = new JTextField(20);
+		JButton sendenButton = new JButton("Senden");
+		sendenButton.addActionListener(new SendenButtonListener());
+		hauptPanel.add(fScroller);
+		hauptPanel.add(sendenButton);
+		hauptPanel.add(sendenButton);
+		netzwerkEinrichten();
+
+		Thread readerThread = new Thread(new EingehendReader());
+		readerThread.start();
+
+		frame.getContentPane().add(BorderLayout.CENTER, hauptPanel);
+		frame.setSize(400,500);
+		frame.setVisible(true);
+
+	}
+
+	public void netzwerkEinrichten() {
+		try {
+			sock = new Socket("127.0.0.1", 5000);
+			InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
+			reader = new BufferedReader(streamReader);
+			writer = new PrintWriter(sock.getOutputStream());
+			System.out.println("Netzwerkverbindung steht");
+		} catch(IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public class SendenButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent ev) {
+			try {
+				writer.println(ausgehend.getText());
+				writer.flush();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+			ausgehend.setText("");
+			ausgehend.requestFocus();
+		}
+	}
+
+	public class EingehendReader implements Runnable {
+		public void run() {
+			String nachricht;
+			try {
+				while ((nachricht=reader.readLine()) != null) {
+					System.out.println("gelesen: " + nachricht);
+					eingehend.append(nachricht + "\n");
+				}
+			} catch(Exception ex) {ex.printStackTrace();}
+		}
+	}
+}
